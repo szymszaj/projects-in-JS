010e0384959db800acd5733106a0fc9071367420
Szymon Zych <szymonzych868@gmail.com>
2023-06-27 22:50:16 +0200

    assigning functions to buttons

--- converting °C & °F/add.js
+++ converting °C & °F/add.js
@@ -13,10 +13,47 @@ const swap = () => {
     if(one.textContent === '°C') {
         one.textContent = '°F'
         two.textContent = '°C'
+        result.textContent = ''
     } else {
         one.textContent = '°C'
         two.textContent = '°F'
+        result.textContent = ''
     }
 }
 
+const fahrToCel = () => {
+    fahrenheit = converter.value * 1.8 + 32 
+    result.textContent = `${converter.value}°C to ${fahrenheit.toFixed(1)}°F`
+    converter.value = ''    
+}
+
+const celToFar = () => {
+    celsius = (converter.value -32) / 1.8
+    result.textContent = `${converter.value}°F to ${celsius.toFixed(1)}°C`
+    converter.value = ''
+}
+
+const conversion = () => {
+    if (converter.value !== ''){
+        if(one.textContent ==='°C') {
+            fahrToCel()
+        } else {
+            celToFar()
+        }
+    } else {
+        result.textContent = 'Musisz podać jakąś wartość'
+    }
+}
+
+
+const reset = () => {
+    converter.value = ''
+    result.textContent = ''
+}
+
 changeBtn.addEventListener('click', swap)
+// convBtn.addEventListener('click', fahrToCel)
+// convBtn.addEventListener('click', celToFar)
+convBtn.addEventListener('click', conversion)
+resetBtn.addEventListener('click', reset)
+ 
