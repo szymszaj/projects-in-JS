4ad156d87a9b5af59cb394948329e9d612207be3
Szymon Zych <szymonzych868@gmail.com>
2023-07-31 16:24:29 +0200

    objects in js

---
+++ objects-in-js/app.js
@@ -0,0 +1,33 @@
+// const myObject = {
+//     key: value,
+//     key: value,
+//     key: value
+// }
+//test1
+const newUser = {
+    name: 'Lisa',
+    age: 23,
+    car: {
+        brand: 'audi',
+        color: 'black'
+    }
+}
+// console.log(newUser.name);
+// console.log(newUser.age);
+// console.log(newUser.car.color);
+// console.log(newUser.car.brand);
+
+console.log(`${newUser.name} jezdzi samochode marki ${newUser.car.brand},
+o kolorze ${newUser.car.color}`);
+
+
+//test2
+const newPeople = {
+    name: 'Marek',
+    age: '20',
+    hobby: 'read the book'
+
+}
+
+console.log(newPeople.age);
+console.log(newPeople.hobby);
