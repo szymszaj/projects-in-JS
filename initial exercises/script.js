f3fc963ac3ec760bd2c823d7b7a84987d1f3488b
Szymon Zych <szymonzych868@gmail.com>
2023-07-31 16:38:11 +0200

    adding and creating functions

--- initial exercises/script.js
+++ initial exercises/script.js
@@ -1,3 +1,5 @@
+
+//próba1
 const add = (x, y) => {
     const score = x + y 
     showScore(score)
@@ -8,3 +10,48 @@ const showScore = score2 => {
 }
 
 add(3, 9)
+//próba2
+const is = (x, y) => {
+    const sth = x + y 
+    showScore(sth)
+}
+
+const showSx = score3 => {
+    console.log(`wynik to:${score3}`);
+}
+
+is(4, 1)
+//próba3
+const toup = (p, q) => {
+    const result = p * q
+    showScore(result)
+}
+
+const show2 = score8 => {
+    console.log(`wynik mnozenia to: ${score8}`);
+}
+
+toup(2, 6)
+
+//próba3
+const milk = (q, z) => {
+    const lorem = q / z 
+    showScore(lorem)
+}
+
+const show1 = score1 => {
+    console.log(`wynik dzielenia:${score1}`);
+}
+
+milk(10, 2)
+
+//próba 4 
+const fast = (s, j, z) => {
+    const result2 = s + j - z
+    showScore(result2)
+}
+
+const show99 = is => {
+    console.log(`wynik:${is}`);
+}
+fast (2, 2, 9)
