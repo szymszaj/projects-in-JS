a169a5e6727dc3f1d51c918a4d0a077b851e1e92
Szymon Zych <szymonzych868@gmail.com>
2023-08-08 23:42:28 +0200

    update/laying code

--- Toast Notification or Snack Bar/script.js
+++ Toast Notification or Snack Bar/script.js
@@ -2,7 +2,6 @@ let toastBox = document.getElementById('toastBox');
 let successMsg = 'Successfully submitted'
 let errorMsg = 'Please fix the error!'
 let invalidMsg = 'Invalid input, check again'
-
 function showToast(msg){
     let toast = document.createElement('div');
     toast.classList.add('toast');
