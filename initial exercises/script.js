a0e1ee0633a4ab67b2f7e6b7556e8c23626d1a5c
Szymon Zych <szymonzych868@gmail.com>
2023-08-08 23:46:40 +0200

    adding a new calculation-based command

--- initial exercises/script.js
+++ initial exercises/script.js
@@ -1,26 +1,23 @@
-
 //próba1
 const add = (x, y) => {
     const score = x + y 
     showScore(score)
 }
-
 const showScore = score2 => {
     console.log(`Wynik to: ${score2}`);
 }
-
 add(3, 9)
+
 //próba2
 const is = (x, y) => {
     const sth = x + y 
     showScore(sth)
 }
-
 const showSx = score3 => {
     console.log(`wynik to:${score3}`);
 }
-
 is(4, 1)
+
 //próba3
 const toup = (p, q) => {
     const result = p * q
@@ -30,7 +27,6 @@ const toup = (p, q) => {
 const show2 = score8 => {
     console.log(`wynik mnozenia to: ${score8}`);
 }
-
 toup(2, 6)
 
 //próba3
@@ -38,11 +34,9 @@ const milk = (q, z) => {
     const lorem = q / z 
     showScore(lorem)
 }
-
 const show1 = score1 => {
     console.log(`wynik dzielenia:${score1}`);
 }
-
 milk(10, 2)
 
 //próba 4 
@@ -50,7 +44,6 @@ const fast = (s, j, z) => {
     const result2 = s + j - z
     showScore(result2)
 }
-
 const show99 = is => {
     console.log(`wynik:${is}`);
 }
@@ -62,11 +55,9 @@ const hover = (s, j, z) => {
     const resultXd = s * j / z;
     show11(resultXd);
 }
-
 const show11 = ddd => {
     console.log(`wynik jest: ${ddd}`);
 }
-
 hover(10, 3, 9);
 
 //próba6 
@@ -74,7 +65,6 @@ const autcome = (o, p) =>{
     const result09 = o - p;
     showScore(result09)
 }
-
 const showJJ = autcome => {
     console.log(`wynik: ${autcome}`);
 }
@@ -89,5 +79,36 @@ const outcome = (o, p) => {
   const showJ = (outcome) => {
     console.log(`wynik: ${outcome}`);
   };
-  
   outcome(5, 3);
+
+
+
+
+  // Próba 8
+const calculateDifference = (a, b) => {
+    const difference = a - b;
+    displayResult(difference);
+};
+
+const displayScore = (value) => {
+    console.log(`Wynik: ${value}`);
+};
+
+calculateDifference(2167, 1205);
+
+// Próba 9
+const calculateSquaredSum = (x, y) => {
+    const result = Math.pow(x + y, 9);
+    displayResult(result);
+};
+
+const displayOutcome = (value) => {
+    console.log(`Wynik: ${value}`);
+};
+
+calculateSquaredSum(5, 3);
+
+
+
+
+
