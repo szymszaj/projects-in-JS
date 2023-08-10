d843e93103f5543a789831c9fe46ee9761036c13
Szymon Zych <szymonzych868@gmail.com>
2023-08-10 22:09:57 +0200

    creating and adding objects

---
+++ objects-in-js/app2.js
@@ -0,0 +1,37 @@
+//adding things to objects
+
+const user = {
+    name: 'Justyna',
+    age: 20,
+    'fav-color': 'czerwony'
+}
+user.country = 'Polska'
+//user.fav-color
+user['fav-color'] = 'niebieski'
+console.log(user);
+
+//proba 1
+const dogName = 'dante'
+const dogAge = 4
+
+const dog = {
+    dogName,
+    dogAge
+
+}
+console.log(dog);
+
+//proba 2
+
+const birdType = 'parrot'
+const birdColor = 'black'
+const birdAge = 2
+
+const bird = {
+    birdType,
+    birdColor,
+    birdAge
+}
+console.log(bird);
+
+//proba 3
