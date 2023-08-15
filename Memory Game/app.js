e5f35da61c6b520500a6d61646b2610468c938ab
Szymon Zych <szymonzych868@gmail.com>
2023-08-15 12:06:58 +0200

    movement when clicking windows

--- Memory Game/app.js
+++ Memory Game/app.js
@@ -4,5 +4,10 @@ for ( var i = 0; i<emojis.length; i++){
     let box = document.createElement('div')
     box.className = 'item';
     box.innerHTML = shuf_emojis[i]
+
+    box.onclick = function(){
+        this.classList.add('boxOpen')
+    }
+
     document.querySelector('.game').appendChild(box);
 }
