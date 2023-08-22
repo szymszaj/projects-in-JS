c2ecf2532a328dc4b91a2bbecd5d2cc889e2cddb
Szymon Zych <szymonzych868@gmail.com>
2023-08-22 15:09:53 +0200

    examples from promisy

--- json-ajax-api/app.js
+++ json-ajax-api/app.js
@@ -10,3 +10,51 @@ btn.addEventListener('click', () => {
 
 .catch(err => console.error(err))
 })
+
+//promisy 
+
+const test  = new Promise((resolve, reject ) => {
+    if(!true) {
+        resolve('jest ok!')
+    } else {
+        reject('błąd')
+    }
+    })
+    test 
+    .then(info => console.log(info))
+    .catch(err => console.error(err));
+
+    //2 
+
+    const conditionalPromise = new Promise ((resolve, reject) => {
+        const randomNumber = Math.random();
+        if(randomNumber > 0.5 ){
+            resolve('warunek spełniony');
+        } else {
+            reject('warunek nie spełniony');
+
+        }
+    });
+
+    conditionalPromise
+    .then(info => console.log(info))
+    .catch(err => console.error(err))
+    
+    //3 
+    const multiStepPromise = new Promise ((resolve, reject) => {
+        const step1 = 'wykonuje zadanie 1 '
+        resolve(step1);
+    })
+
+    .then(result => {
+        console.log(result);
+        const step2 = 'wykonanie kroku 2'
+        return step2;
+    })
+    .then(result => {
+        console.log(result);
+        const step3 ='wynkonanie zadania 3 '
+        return step3;
+    })
+
+    .catch(err => console.error(err))
