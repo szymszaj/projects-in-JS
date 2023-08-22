f1492301adc478469d0e6011ccf298fdf5d4e6c0
Szymon Zych <szymonzych868@gmail.com>
2023-08-22 15:30:28 +0200

    Async & await

--- json-ajax-api/app.js
+++ json-ajax-api/app.js
@@ -13,16 +13,16 @@ btn.addEventListener('click', () => {
 
 //promisy 
 
-const test  = new Promise((resolve, reject ) => {
-    if(!true) {
-        resolve('jest ok!')
-    } else {
-        reject('błąd')
-    }
-    })
-    test 
-    .then(info => console.log(info))
-    .catch(err => console.error(err));
+// const test  = new Promise((resolve, reject ) => {
+//     if(!true) {
+//         resolve('jest ok!')
+//     } else {
+//         reject('błąd')
+//     }
+//     })
+//     test 
+//     .then(info => console.log(info))
+//     .catch(err => console.error(err));
 
     //2 
 
@@ -58,3 +58,54 @@ const test  = new Promise((resolve, reject ) => {
     })
 
     .catch(err => console.error(err))
+
+
+    //4
+
+    const checkAge = age => {
+        return new Promise ((resolve, reject ) => {
+            if (age >= 18) {
+                resolve()
+            } else {
+                reject('Masz za mało lat!')
+            }
+        })
+    }
+
+    const doubleCheck = () => {
+        return new Promise(resolve => {
+            console.log('sprawdzam jeszcze raz..');
+            setTimeout(() => {
+                resolve('faktyczne wiek sie zgadza.')
+            },1000)
+        })
+    }
+
+
+    // checkAge(20) 
+    // .then(() => {
+    //     console.log('chyba mozesz wejsc.');
+    //     return doubleCheck()
+    // })
+
+    // .then (res => console.log(res))
+    // .then(() => {
+    //     console.log('weryfikacja zkonczona');
+    // })
+    // .catch(error => console.error(error))
+
+    // async&await 
+
+    async function test() {
+        try {
+            await checkAge(22)
+            console.log('chyba mozesz wejsc');
+            await doubleCheck()
+            console.log('faktycznie wiek jest ok');
+            console.log('weryfikacja zakonczona ');
+        } catch {
+            console.error('bład');
+        }
+    }
+    test()
+
