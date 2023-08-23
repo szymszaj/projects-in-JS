0a0e188f91689cad1a952068b09e9f9b748812c8
Szymon Zych <szymonzych868@gmail.com>
2023-08-23 11:37:03 +0200

    code update /0 revisions

--- Toast Notification or Snack Bar/script.js
+++ Toast Notification or Snack Bar/script.js
@@ -2,21 +2,21 @@ let toastBox = document.getElementById('toastBox');
 let successMsg = '<i class="fa-solid fa-circle-check"></i> Successfully submitted'
 let errorMsg = '<i class="fa-solid fa-circle-xmark"></i> Please fix the error!'
 let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i> Invalid input, check again'
-function showToast(msg){
+function showToast(msg) {
     let toast = document.createElement('div');
     toast.classList.add('toast');
     toast.innerHTML = msg;
     toastBox.appendChild(toast);
 
-    if (msg.includes('error')){
+    if (msg.includes('error')) {
         toast.classList.add('error');
     }
-    if (msg.includes('Invalid')){
+    if (msg.includes('Invalid')) {
         toast.classList.add('invalid');
     }
 
 
-    setTimeout(()=>{
+    setTimeout(() => {
         toast.remove();
-    },6000);
+    }, 6000);
 }
