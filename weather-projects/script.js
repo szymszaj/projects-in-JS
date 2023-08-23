76da486b927609a28a6bd5bf25d87510bc158d81
Szymon Zych <szymonzych868@gmail.com>
2023-08-23 13:38:31 +0200

    implement a weather icon when it occurs

--- weather-projects/script.js
+++ weather-projects/script.js
@@ -21,14 +21,35 @@ const getWeather = () => {
         const temp = res.data.main.temp
         const hum = res.data.main.humidity
         const status = Object.assign({}, ...res.data.weather)
-   
+
+        warning.textContent = ''
+        input.value = ''
+
         weather.textContent = status.main
         cityName.textContent = res.data.name
         temperature.textContent = Math.floor(temp) + '°C'
         humidity.textContent = hum + '%'
 
-
+        //wyswietlanie ikonek pogodowych 
+        if (status.id >= 200 && status.id < 300) {
+            photo.setAttribute('src', './img/thunderstorm.png')
+        } else if (status.id >= 300 && status.id < 400) {
+            photo.setAttribute('src', './img/drizzle.png')
+        } else if (status.id >= 500 && status.id < 600) {
+            photo.setAttribute('src', './img/rain.png')
+        } else if (status.id >= 600 && status.id < 700) {
+            photo.setAttribute('src', './img/ice.png')
+        } else if (status.id >= 700 && status.id < 800) {
+            photo.setAttribute('src', './img/fog.png')
+        } else if (status.id === 800) {
+            photo.setAttribute('src', './img/sun.png')
+        } else if (status.id >= 800 && status.id < 900) {
+            photo.setAttribute('src', './img/cloud.png')
+        } else {
+            photo.setAttribute('src', './img/unknown.png')
+        }
     })
+    .catch(() => (warning.textContent = 'Wpisz poprawną nazwę miasta!'))
 }
 getWeather()
 button.addEventListener('click', getWeather)
