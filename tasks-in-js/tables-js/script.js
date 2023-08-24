ebbd87fca723a7c89b5106bfa645575b12f929fd
Szymon Zych <szymonzych868@gmail.com>
2023-08-24 13:15:21 +0200

    exercises on boards/ application of methods

--- tasks-in-js/tables-js/script.js
+++ tasks-in-js/tables-js/script.js
@@ -33,17 +33,56 @@ console.log(...drink);
 
 const zwierzeta = ['Kot', 'Pies', 'Królik', 'Chomik'];
 
-console.log(zwierzeta[0]); 
-console.log(zwierzeta[2]); 
-console.log(zwierzeta.length); 
+console.log(zwierzeta[0]);
+console.log(zwierzeta[2]);
+console.log(zwierzeta.length);
 
-zwierzeta.push('Ptak'); 
-console.log(zwierzeta); 
+zwierzeta.push('Ptak');
+console.log(zwierzeta);
 
-zwierzeta.pop(); 
-console.log(zwierzeta); 
+zwierzeta.pop();
+console.log(zwierzeta);
 
 console.log(zwierzeta.indexOf('Pies'));
 
-zwierzeta[1] = 'Owczarek Niemiecki'; 
+zwierzeta[1] = 'Owczarek Niemiecki';
 console.log(zwierzeta);
+
+
+const numbers1 = [1, 2, 3, 4, 5]
+function multiply(x) {
+    return x * 2
+}
+const newNumbers = numbers1.map(multiply)
+console.log(numbers1);
+console.log(newNumbers);
+
+numbers1.push(6, 7, 8)
+
+const abc = ['a', 'b', 'c', true]
+
+
+const newAbc = numbers1.concat(abc) //łaczenie tablic 
+console.log(newAbc);
+
+
+//zadanie 
+const numbers2 = [0, 0, 1, 1, 2, 2, 2]
+const colors = ['red', 'green', 'blue', true, 123]
+
+const numbers3 = numbers2.slice(0, 2)
+console.log(numbers3);
+
+const randomStuff = colors.splice(-2)
+console.log(colors);
+console.log(randomStuff);
+
+
+//metody 
+const numbersX = [0, 23, 48, 175, 2, 32, 11, 232, 44, 55]
+function num(x) {
+    return x % 2 === 0
+}
+console.log(numbersX.filter(num));
+
+
