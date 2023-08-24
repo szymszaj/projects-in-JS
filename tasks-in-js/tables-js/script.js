2693c91070f79448f3cd132ef49fbf69651e3b4f
Szymon Zych <szymonzych868@gmail.com>
2023-08-24 14:15:01 +0200

    tasks on the boards

--- tasks-in-js/tables-js/script.js
+++ tasks-in-js/tables-js/script.js
@@ -65,7 +65,7 @@ const abc = ['a', 'b', 'c', true]
 const newAbc = numbers1.concat(abc) //łaczenie tablic 
 console.log(newAbc);
 
-
+console.log('--------');
 //zadanie 
 const numbers2 = [0, 0, 1, 1, 2, 2, 2]
 const colors = ['red', 'green', 'blue', true, 123]
@@ -77,7 +77,7 @@ const randomStuff = colors.splice(-2)
 console.log(colors);
 console.log(randomStuff);
 
-
+console.log('--------');
 //metody 
 const numbersX = [0, 23, 48, 175, 2, 32, 11, 232, 44, 55]
 function num(x) {
@@ -86,3 +86,48 @@ function num(x) {
 console.log(numbersX.filter(num));
 
 
+const letters = ['c', 'd']
+letters.unshift('a', 'b')
+console.log(letters);
+
+letters.push('e', 'f')
+console.log(letters);
+
+console.log(letters.includes('c'));
+
+console.log('--------');
+
+const numb = [1, 2, 3, 4, 9, 10]
+const meal = ['pizza', 'burger', 'pasta']
+
+const newArr = [...numb, ...meal]
+console.log(newArr);
+
+
+console.log('--zadanie--');
+
+const numbers4 = [1, 5, 13, 26, 48]
+function multiply(x) {
+    return x * 5
+}
+const newNumbers4 = numbers4.map(multiply)
+console.log(newNumbers4);
+
+for (const number of newNumbers4) {
+    if (number % 2 === 0) {
+        console.log(`liczba parzysta: ${number}`);
+    } else {
+        console.log(`liczba nieparzysta: ${number}`)
+    }
+}
+
+
+const color1 = ['szary']
+
+color1.unshift('czarny')
+color1.push('biały')
+
+
+for (let i = 0; i < color1.length; i++) {
+    console.log(`moj ulubiony kolor to: ${color1[i].toUpperCase()}`)
+}
