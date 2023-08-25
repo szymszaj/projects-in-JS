db1e833be817012d84be2dab3eda85ce1508bb60
Szymon Zych <szymonzych868@gmail.com>
2023-08-25 13:36:28 +0200

    displaying the second object

--- task-in-modal/main.js
+++ task-in-modal/main.js
@@ -2,6 +2,8 @@ feather.replace();
 
 const actionBtn = document.querySelector(".action-btn");
 
+
+
 const modal = (body) => {
     return `<div class="modal">
     <p class="modal-title info">
@@ -12,27 +14,44 @@ const modal = (body) => {
       ${body.description}
     </p>
     <div class="modal-buttons">
-        <button class="modal-accept">Akceptuj</button>
-        <button class="modal-decline">Anuluj</button>
+        <button class="modal-accept">${body.accept_btn}</button>
+        <button class="modal-decline">${body.decline_btn}</button>
     </div>
     <button class="modal-close-icon"> <i data-feather="x"></i></button>
   </div>`
 }
+
+const createModal = (value) => {
+    const modalContainer = document.createElement('div');
+    modalContainer.className = "modal-container";
+    modalContainer.innerHTML = modal(value);
+    document.body.appendChild(modalContainer);
+    feather.replace();
+}
 const addToBaslet = () => {
     console.log("dodano do oferty");
 
-    const modalContainer = document.createElement('div');
-    modalContainer.className = "modal-container";
-    modalContainer.innerHTML = modal({
+    createModal({
         title: "Lorem ipsum dolor title",
-        description: "Lorem ipsum, dolor sit amet consectetur adipisicing."
-    });
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+        accept_btn: "Zaakceptuj",
+        decline_btn: "Anuluj"
+    })
 
-    document.body.appendChild(modalContainer);
-    feather.replace();
+
 
 };
 
+const actionTwoBtn = document
+    .querySelector(".action-two-btn")
+    .addEventListener("click", () => {
+        createModal({
+            title: "drugi tekst modala",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat enim excepturi maxime molestiae qui illo!",
+            accept_btn: "OK",
+            decline_btn: "NIE"
+        })
+    });
 
 
 actionBtn.addEventListener("click", addToBaslet)
