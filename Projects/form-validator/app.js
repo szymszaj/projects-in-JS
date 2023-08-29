c1f52c90797d0be5c74fc93090811e98b01859e0
Szymon Zych <szymonzych868@gmail.com>
2023-08-29 11:35:12 +0200

    error display

---
+++ Projects/form-validator/app.js
@@ -0,0 +1,49 @@
+const username = document.querySelector('#username')
+const pass = document.querySelector('#password')
+const pass2 = document.querySelector('#password2')
+const email = document.querySelector('#email')
+const sendBtn = document.querySelector('.send')
+const clearBtn = document.querySelector('.clear')
+const popup = document.querySelector('.popup')
+
+const showError = (input, msg) => {
+
+
+    const formBox = input.parentElement;
+    const errorMsg = formBox.querySelector('.error-text');
+    formBox.classList.add('error')
+
+    errorMsg.textContent = msg;
+}
+
+const clearError = input =>{
+    const formBox = input.parentElement;
+    formBox.classList.remove('error')
+}
+
+
+const checkForm = input => {
+    input.forEach(el => {
+        if (el.value === '') {
+            showError(el, el.placeholder)
+        } else {
+            clearError(el)
+        }
+    })
+}
+
+sendBtn.addEventListener('click', e => {
+    e.preventDefault();
+
+    checkForm([username, pass, pass2, email])
+})
+
+
+clearBtn.addEventListener('click', e => {
+    e.preventDefault();
+
+    [username, pass, pass2, email].forEach(el => {
+        el.value = '';
+    });
+
+})
