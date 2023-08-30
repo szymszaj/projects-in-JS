d91b10eccda331d619979553c8dff2ded558bb0e
Szymon Zych <szymonzych868@gmail.com>
2023-08-30 15:45:50 +0200

    adding function call

---
+++ Projects/Magic-Ball/main.js
@@ -0,0 +1,37 @@
+const ball = document.querySelector('img')
+const input = document.querySelector('input')
+const answer = document.querySelector('.answer')
+const error = document.querySelector('.error')
+
+const answerArr = ['Tak!', 'Nie', 'po co chcesz to wiedzieć', ' nie wiem', 'Ciężko powiedzieć', 'Nie chcesz wiedzieć',
+    'sam to zrób', 'zapytaj mamy', 'nie lubie tego typu pytań', 'Może', 'chyba ty'];
+
+
+
+const checkInput = () => {
+    if (input.value !== '' && input.value.slice(-1) === '?') {
+        generatorAnswer();
+        error.textContent = '';
+    } else if (input.value !== '' && input.value.slice(-1) !== '?') {
+        error.textContent = 'Pytanie musi być zakończone znakiem "?".';
+        answer.textContent = ''
+    } else {
+        error.textContent = 'Musisz zadać jakieś pytanie!';
+        answer.textContent = ''
+    }
+
+    ball.classList.remove('shake-animaton');
+}
+
+const generatorAnswer = () => {
+
+    const randomIndex = Math.floor(Math.random() * answerArr.length);
+    answer.innerHTML = `<span>Odpowiedz:</span> ${answerArr[randomIndex]}`
+
+};
+
+ball.addEventListener('click', checkInput)
+
+
+
+
