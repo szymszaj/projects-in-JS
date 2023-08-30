59bcc15d5a58e8660dcdbd8ce7e13b3daa558d25
Szymon Zych <szymonzych868@gmail.com>
2023-08-30 12:35:00 +0200

    email address verification

--- Projects/form-validator/app.js
+++ Projects/form-validator/app.js
@@ -44,6 +44,18 @@ const checkPassword = (pass1, pass2) => {
     }
 }
 
+
+const checkEmail = emai => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+
+    if(re.test(email.value)){
+        clearError(email)
+    } else {
+        showError(email, 'E-mail jest niepoprawny')
+    }
+}
+
 sendBtn.addEventListener('click', e => {
     e.preventDefault();
 
@@ -52,6 +64,8 @@ sendBtn.addEventListener('click', e => {
     checkLength(username, 3)
     checkLength(pass, 8)
     checkPassword(pass, pass2)
+    checkEmail(email)
+
 })
 
 
