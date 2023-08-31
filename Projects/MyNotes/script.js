cf132035852cfb59694e396a03a3c0c37011f6e1
Szymon Zych <szymonzych868@gmail.com>
2023-08-31 16:33:14 +0200

    adding closing and opening the form

---
+++ Projects/MyNotes/script.js
@@ -0,0 +1,28 @@
+const addBtn = document.querySelector('.add')
+const saveBtn = document.querySelector('.save')
+const cancelBtn = document.querySelector('.cancel')
+const deleteBtns = document.getElementsByClassName('delete-note')
+const deleteAllBtn = document.querySelector('.delete-all')
+
+const noteArea = document.querySelector('.note-area')
+const notePanel = document.querySelector('.note-panel')
+const category = document.querySelector('#category')
+const textarea = document.querySelector('#text')
+const error = document.querySelector('.error')
+let selectedValue;
+
+let cardID = 0;
+
+const openPanel = () => {
+    notePanel.style.display = 'flex';
+}
+
+const closePanel = () => {
+    notePanel.style.display = 'none'
+    error.style.visibility = 'hidden'
+    textarea.value = '';
+    category.selectedIndex = 0
+}
+
+addBtn.addEventListener('click', openPanel);
+cancelBtn.addEventListener('click', closePanel)
