8e8b38e77084a075adf64534907a47608f77f25b
Szymon Zych <szymonzych868@gmail.com>
2023-09-03 14:51:07 +0200

    adding functions to the "archive" and "reset" buttons

--- Projects/stopwatch-that-changes-color/index.js
+++ Projects/stopwatch-that-changes-color/index.js
@@ -1,7 +1,7 @@
 const startBtn = document.querySelector('.start')
 const pauseBtn = document.querySelector('.pause')
 const stopBtn = document.querySelector('.stop')
-const resetBtn = document.querySelector('reset')
+const resetBtn = document.querySelector('.reset')
 const historyBtn = document.querySelector('.start')
 const stopwatch = document.querySelector('.stopwatch')
 const time = document.querySelector('.time')
@@ -46,17 +46,9 @@ const handleStop = () => {
         time.style.visibility = 'visible'
         timeArr.push(stopwatch.textContent)
         console.log(timeArr);
-
-
-
-        // time.style.fontWeight = 'bold'
+    // time.style.fontWeight = 'bold'
     }
-
-    clearInterval(countTime)
-    stopwatch.textContent = '0:00'
-    timeList.textContent = ''
-    seconds = 0
-    minutes = 0
+    clearStuff();
 }
 
 
@@ -64,7 +56,35 @@ const handlePause = () => {
     clearInterval(countTime)
 }
 
+const handleReset = () => {
+    time.style.visibility = 'hidden'
+    timeArr = []
+    clearStuff();
+}
+
+
+const clearStuff = () => {
+    clearInterval(countTime)
+    stopwatch.textContent = '0:00'
+    timeList.textContent = ''
+    seconds = 0
+    minutes = 0
+}
+const showHistory = () => {
+    let num = 1
+    timeList.textContent = ''
+    timeArr.forEach(time => {
+        const newTime = document.createElement('li')
+        newTime.innerHTML = `Pomiar nr ${num}: <span>${time}</span>`
+        timeList.appendChild(newTime)
+        num++
+    })
+
+}
+
 
 startBtn.addEventListener('click', handleStart)
 pauseBtn.addEventListener('click', handlePause)
 stopBtn.addEventListener('click', handleStop)
+resetBtn.addEventListener('click', handleReset)
+historyBtn.addEventListener('click', showHistory)
