cc05545ac98891dcc3fc8251248a9d35fea388e2
Szymon Zych <szymonzych868@gmail.com>
2023-09-06 13:23:13 +0200

    opening panel for adding items

--- Projects/finance-app/main.js
+++ Projects/finance-app/main.js
@@ -19,3 +19,28 @@ let ID = 0;
 let categoryIcon;
 let selectedCategory;
 let moneyArr = [0];
+
+const showPanel = () => {
+    addTransactionPanel.style.display = 'flex'
+}
+const closePanel = () => {
+    addTransactionPanel.style.display = 'none'
+    clearInputs()
+}
+const checkForm = () => {
+    if (nameInput.value !== '' && amountInput.value !== '' && categorySelect.value !== 'none') {
+        console.log('ok');
+    } else {
+        alert('Wypełnij wszystkie pola!')
+    }
+}
+const clearInputs = () => {
+    nameInput.value = '';
+    amountInput.value = '';
+    categorySelect.selectedIndex = 0;
+}
+
+//wywoływanie funkcji
+addTransaction.addEventListener('click', showPanel)
+cancelBtn.addEventListener('click', closePanel)
+saveBtn.addEventListener('click', checkForm)
