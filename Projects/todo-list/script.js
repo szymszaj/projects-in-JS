347f3a9a22e017b42fa352220208bfb1c2acc635
Szymon Zych <szymonzych868@gmail.com>
2023-09-08 17:21:10 +0200

    downloading elements and adding optimization

---
+++ Projects/todo-list/script.js
@@ -0,0 +1,41 @@
+let $todoInput  
+let $alertInput
+let $addBtn
+let $ulList
+let $newTask
+let $allTasks
+let $idNumber = 0
+let $popup
+let $popupInfo
+let $editedTodo
+let $popupInput
+let $addPopupBtn
+let $closeTodoBtn
+
+const main = () => {
+    prepareDOMElements();
+    prepareDOMEvents();
+}
+
+const prepareDOMElements = () => {
+    $todoInput = document.querySelector('.todo-input');
+    $alertInfo = document.querySelector('.alert-info');
+    $addBtn = document.querySelector('.add-btn');
+    $ulList = document.querySelector('.todo-list ul');
+    $allTasks = document.getElementsByTagName('li');
+    $popup = document.querySelector('.popup');
+    $popupInfo = document.querySelector('.popup-info');
+    $popupInput = document.querySelector('.popup-input');
+    $addPopupBtn = document.querySelector('.accept');
+    $closeTodoBtn = document.querySelector('.cancel');
+}
+
+const prepareDOMEvents = () => {
+    $addBtn.addEventListener('click', addNewTask);
+}
+
+const addNewTask = () => {
+    console.log('ok');
+}
+
+document.addEventListener('DOMContentLoaded', main);
