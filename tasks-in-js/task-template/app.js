9cb448bf77bb3244c968419183478f4c07fca11b
Szymon Zych <szymonzych868@gmail.com>
2023-09-09 11:22:43 +0200

    template

--- tasks-in-js/task-template/app.js
+++ tasks-in-js/task-template/app.js
@@ -3,16 +3,31 @@ const wrapper = document.querySelector('.wrapper')
 const squareTemp = document.querySelector('.square-temp')
 
 let num = 1
-const addMoreSquares = () => {
-    const square = document.createElement('div')
-    square.classList.add('square')
 
-    square.innerHTML = `
-    <div class="square">
-            <p class="square-number">${num}</p>
-        </div>
-    `
+//BEFORE
+// const addMoreSquares = () => {
+//     const square = document.createElement('div')
+//     square.classList.add('square')
+
+//     square.innerHTML = `
+//     <div class="square">
+//             <p class="square-number">${num}</p>
+//         </div>
+//     `
+//     wrapper.appendChild(square)
+//     num++
+// }
+
+
+//after
+const addMoreSquares = () => {
+    const square = squareTemp.content.cloneNode(true)
+
+    square.querySelector('.square-number').textContent = num
     wrapper.appendChild(square)
+
     num++
+
+
 }
 btn.addEventListener('click', addMoreSquares)
