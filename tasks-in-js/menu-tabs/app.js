c4745f5c5afc536760c94ccb8b10998103323d21
Szymon Zych <szymonzych868@gmail.com>
2023-09-10 16:03:27 +0200

    tabs menu how it works/creating a simple pattern

---
+++ tasks-in-js/menu-tabs/app.js
@@ -0,0 +1,12 @@
+const menuSection = document.querySelectorAll('.menu-section')
+const menuTabs = document.querySelectorAll('.menu-tab')
+
+const showInfo = (id) => {
+    menuSection.forEach(section => section.style.display = 'none')
+    menuTabs.forEach(tab => tab.classList.remove('menu-tab-active'))
+
+    document.getElementById(id).style.display = 'block'
+
+    const currentActiveButton = document.querySelector(`[data-id=${id}]`)
+    currentActiveButton.classList.add('menu-tab-active')
+}
