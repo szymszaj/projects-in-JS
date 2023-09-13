0d61aa714d2d55c724d7f571e7cfb444d4d7e59a
Szymon Zych <szymonzych868@gmail.com>
2023-09-13 13:50:57 +0200

    importing files

---
+++ tasks-in-js/import, export/app.js
@@ -0,0 +1,9 @@
+import MyUser , * as userModule from'./index.js'
+
+console.log(MyUser);
+console.log(userModule);
+
+
+// console.log(zmiennaA);
+// funkcjaB(); 
+// const instancjaC = new KlasaC();
---
+++ tasks-in-js/import, export/index.js
@@ -0,0 +1,19 @@
+
+export const user = {name: 'Jon'}
+
+export function sayHi() {
+    console.log('HIII');
+}
+
+// export const zmiennaA = "To jest zmienna A";
+
+
+// export function funkcjaB() {
+//   console.log("To jest funkcja B");
+// }
+
+// export default class KlasaC {
+//   constructor() {
+//     console.log("To jest klasa C");
+//   }
+// }
