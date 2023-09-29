10225cb7b1292a682c79c7c6508d70ea9fee97af
Szymon Zych <szymonzych868@gmail.com>
2023-09-29 18:37:21 +0200

    example of building a board

---
+++ tasks-in-js/forEach-map/app.js
@@ -0,0 +1,20 @@
+const people = [
+    { name: 'Jan', lastName: 'Kowalski', country: 'Polska', solary: 1250 },
+    { name: 'Anna', lastName: 'Brown', country: 'Niemcy', solary: 9855 },
+    { name: 'Szymon', lastName: 'Smith', country: 'USA', solary: 500 },
+    { name: 'Wojetek', lastName: 'Silva', country: 'UK', solary: 45000 },
+    { name: 'Justyna', lastName: 'Enmena', country: 'Francja', solary: 8900 },
+    { name: 'Kamil', lastName: 'Taylor', country: 'UK', solary: 2300 },
+    { name: 'Luis', lastName: 'Dubois', country: 'Polska', solary: 500 },
+    { name: 'Sarah', lastName: 'Joric', country: 'UK', solary: 57000 },
+    { name: 'Mila', lastName: 'Klars', country: 'Australia', solary: 50300 },
+];
+
+const logPersonInfo = (person) => {
+    console.log(`${person.name} ${person.lastName}, kraj: ${person.country}`);
+}
+
+for (let i = 0; i < people.length; i++) {
+    const person = people[i]
+    logPersonInfo(person)
+}
