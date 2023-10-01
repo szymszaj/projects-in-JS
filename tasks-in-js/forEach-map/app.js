74bf7692e179da6617b4248f4c6938b4df26b6ec
Szymon Zych <szymonzych868@gmail.com>
2023-10-01 18:22:28 +0200

    map

--- tasks-in-js/forEach-map/app.js
+++ tasks-in-js/forEach-map/app.js
@@ -1,7 +1,7 @@
 const people = [
     { name: 'Jan', lastName: 'Kowalski', country: 'Polska', solary: 1250 },
     { name: 'Anna', lastName: 'Brown', country: 'Niemcy', solary: 955 },
-    { name: 'Szymon', lastName: 'Smith', country: 'USA', solary: 500 },
+    { name: 'Szymon', lastName: 'Smith', country: 'USA', solary: 1500 },
     { name: 'Wojetek', lastName: 'Silva', country: 'UK', solary: 45000 },
     { name: 'Justyna', lastName: 'Enmena', country: 'Francja', solary: 8900 },
     { name: 'Kamil', lastName: 'Taylor', country: 'UK', solary: 2300 },
@@ -13,7 +13,6 @@ const people = [
 const logPersonInfo = (person, index, arr) => {
     console.log(`${index + 1} z ${arr.length}: ${person.name} ${person.lastName}, kraj: ${person.country}`);
 }
-
 // for (let i = 0; i < people.length; i++) {
 //     const person = people[i]
 //     logPersonInfo(person, i, people)
@@ -27,7 +26,6 @@ people.forEach((induce) => {
 //     logPersonInfo(person, index, arr);
 // })
 
-
 people.forEach(logPersonInfo)
 
 // const richPeople = []
@@ -37,15 +35,28 @@ people.forEach(logPersonInfo)
 //         richPeople.push(person)
 //     }
 // }
-
-
 console.log("Rich people");
 
-
 people.filter((person) => person.solary > 5000).forEach(logPersonInfo)
-
 
 const polishPeople = people.filter((person) => person.country === "Polska")
 console.log("Polish people");
 console.log(polishPeople);
 
+//Przekształcenie 
+
+// const yearlySalaries = []
+// for(let i = 0; i < people.length; i++){
+//     const person = people[i]
+//     yearlySalaries.push(person.solary *12)
+// }
+
+//map
+const yearlySalaries = people.map((person) => person.solary *12)
+console.log(yearlySalaries);
+
+
+const peopleWithYearlySalaries = people
+    .map((person) => ({...person, yearlySalaries: person.solary *12}));
+    
+console.log(peopleWithYearlySalaries);
