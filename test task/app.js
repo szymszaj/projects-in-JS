958a175a4550f297280f05d9e959376d2b3b7b40
Szymon Zych <szymonzych868@gmail.com>
2023-10-29 08:56:47 +0100

    checking the diagram for the task-algorithm

---
+++ test task/app.js
@@ -0,0 +1,21 @@
+var popyt = 123; // Przykładowa wartość popytu
+var podaż = 431; // Przykładowa wartość podaży
+var faktycznaProdukcja = 0; // Przykładowa początkowa wartość faktycznej produkcji
+var potencjalnaProdukcja = 120; // Przykładowa wartość poziomu potencjalnego
+
+
+if (faktycznaProdukcja < potencjalnaProdukcja) {
+    if (popyt >= podaż) {
+        // Jeśli popyt jest większy lub równy podaży, produkcja jest determinowana przez popyt
+        faktycznaProdukcja = popyt;
+    } else {
+        // W przeciwnym przypadku, gdy popyt jest mniejszy od podaży, produkcja jest mniejsza niż potencjalna
+        // W tym przypadku przedsiębiorstwa dostarczają tylko tyle produkcji, ile jest zapotrzebowania
+        faktycznaProdukcja = podaż;
+    }
+} else {
+    console.log("Faktyczna produkcja jest już równa lub przekracza poziom potencjalny.");
+}
+
+// Komenda kończąca program
+console.log("Faktyczna wielkość produkcji: " + faktycznaProdukcja);
