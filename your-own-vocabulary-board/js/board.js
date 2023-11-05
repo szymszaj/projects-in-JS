4d08b1a7a828e4db21c0124b426067f966c9f756
Szymon Zych <szymonzych868@gmail.com>
2023-11-05 17:17:51 +0100

    adding an array to change

---
+++ your-own-vocabulary-board/js/board.js
@@ -0,0 +1,18 @@
+const words = [
+    { polish: "firma macierzysta", english: "parent company" },
+    { polish: "prowadzić biznes", english: "run a business" },
+    { polish: "jednostka zależna", english: "subsidiary" },
+    { polish: "autor", english: "author" },
+    { polish: "report to somebody", english: "raportować komuś" },
+    { polish: "look after", english: "opiekować się" },
+    { polish: "deal with somebody", english: "zajmować się kimś" },
+    { polish: "be responsible for something", english: "być odpowiedzialnym za coś" },
+    { polish: "be in charge of somebody", english: "być szefem kogoś" },
+    { polish: "be retired", english: "być na emeryturze" },
+    { polish: "work shifts", english: "pracować na zmiany" },
+    { polish: "working hours", english: "godziny pracy" },
+    { polish: "working days", english: "dni robocze" },
+    { polish: "zaniedbywać", english: "neglect" }
+];
+
+// export { words };
