c3be20629e4283c224e2bfe25467668ca67528ca
Szymon Zych <szymonzych868@gmail.com>
2023-11-09 13:14:20 +0100

    clearing commands

--- Sentence-building-application/app.js
+++ Sentence-building-application/app.js
@@ -1,20 +1,32 @@
+// Pobieramy referencje do elementów HTML
 const dataInput = document.getElementById('data-input');
 const analyzeButton = document.getElementById('analyze-button');
 const analysisResults = document.getElementById('analysis-results');
+const cancelBtn = document.getElementById('cancelbtn');
 
+// Funkcja generująca zdanie
 const generateSentence = (inputText) => {
+  // Rozdzielamy tekst na słowa
   const words = inputText.split(/\s+/).filter(word => word.length > 0);
 
   if (words.length === 0) {
     return "Proszę wprowadzić co najmniej jedno słowo.";
   }
 
+  // Generujemy zdanie na podstawie wprowadzonych słów
   const result = "Oto zdanie z wprowadzonych słów: " + words.join(' ') + ".";
   return result;
 };
 
+// Obsługa przycisku "Twórz"
 analyzeButton.addEventListener('click', () => {
   const inputText = dataInput.value;
   const generatedSentence = generateSentence(inputText);
   analysisResults.textContent = generatedSentence;
+});
+
+// Obsługa przycisku "Cofnij"
+cancelBtn.addEventListener('click', () => {
+  dataInput.value = ''; // Czyścimy pole do wprowadzania tekstu
+  analysisResults.textContent = ''; // Czyścimy wyniki analizy
 });
