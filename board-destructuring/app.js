73c3b6d54d0988961367075de301e46ae1e06e05
Szymon Zych <szymonzych868@gmail.com>
2023-12-19 20:08:45 +0100

    adding new board and desreucturing

--- board-destructuring/app.js
+++ board-destructuring/app.js
@@ -12,3 +12,19 @@ let b = 2;
 
 console.log(a); // 2
 console.log(b); // 1
+// Destrukturyzacja obiektów
+const product = {
+    title: 'Fiszki z JavaScript',
+    price: '149',
+    shippingCost: '14',
+    available: true,
+  };
+  
+  const { title, shippingCost } = product;
+  
+  console.log(title); // Fiszki z JavaScript
+  console.log(shippingCost); // 14
+  
+  // Destrukturyzacja obiektu przekazywanego do funkcji jako argument
+  const printProduct = ({ title, price }) => console.log(`${title}|${price}PLN`);
+  printProduct(product);
