4e94d7b79291e9a5af45410823c163e6e077c4b0
Szymon Zych <szymonzych868@gmail.com>
2023-12-22 18:44:46 +0100

    board

--- board-destructuring/app.js
+++ board-destructuring/app.js
@@ -1,40 +1,37 @@
 // Destrukturyzacja tablic
-const array = ["React", "Angular", "Vue"];
+const array = ['React', 'Angular', 'Vue'];
 
 const [firstElement, secondElementWithOurCustomName] = array;
-console.log(firstElement); // React
-console.log(secondElementWithOurCustomName);
+console.log(firstElement); 
+console.log(secondElementWithOurCustomName); 
 
+// Zamiania miejscami elementów w tablicy przy pomocy destrukturyzacji
 let a = 1;
 let b = 2;
 
 [a, b] = [b, a];
 
-console.log(a);
-console.log(b);
-<<<<<<< HEAD
+console.log(a); 
+console.log(b); 
 
-=======
-// Destrukturyzacja obiektów
->>>>>>> 87845eeb35087d785843156e9a52eda8818b7164
+
 const product = {
-	title: "Fiszki z JavaScript",
-	price: "149",
-	shippingCost: "14",
-	available: true,
+  title: 'Fiszki z JavaScript',
+  price: '149',
+  shippingCost: '14',
+  available: true,
 };
 
 const { title, shippingCost } = product;
 
-console.log(title);
-console.log(shippingCost);
+console.log(title); 
+console.log(shippingCost); 
 
-// Destrukturyzacja obiektu przekazywanego do funkcji jako argument
 const printProduct = ({ title, price }) => console.log(`${title}|${price}PLN`);
 printProduct(product);
 
-<<<<<<< HEAD
+
+
+
 const { price, ...rest } = product;
 console.log(rest);
-=======
->>>>>>> 87845eeb35087d785843156e9a52eda8818b7164
