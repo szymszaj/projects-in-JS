8135c647392ca40a2caffae9ed83cc4d8347d0b0
Szymon Zych <szymonzych868@gmail.com>
2023-12-26 17:10:45 +0100

    nesting of arrays

--- test task/reduce/app.js
+++ test task/reduce/app.js
@@ -1,8 +1,18 @@
 const products = [{ price: 1 }, { price: 2 }, { price: 3 }];
-const startValue = 0;
+// const startValue = 0;
 
 const sum = products.reduce(
   (sum, currentProduct) => sum + currentProduct.price,
   startValue
 );
 console.log(sum);
+
+const elements = [1, 2, 3, 4, 5, 6];
+const startValue = [[], []];
+const groupedValues = elements
+  .reduce((sum, element) => element < 3 ?
+  [[...sum[0], element], sum[1]]
+  :
+  [sum[0], [...sum[1], element]],
+  );
+  console.log(groupedValues);
