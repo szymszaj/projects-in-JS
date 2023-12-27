30f516e61dfcf39b1ee68a6b7d14d4803fffa3a3
Szymon Zych <szymonzych868@gmail.com>
2023-12-27 21:37:01 +0100

    upadte

--- test task/reduce/app.js
+++ test task/reduce/app.js
@@ -1,13 +1,9 @@
 const products = [{ price: 1 }, { price: 2 }, { price: 3 }];
-// const startValue = 0;
-
 const sum = products.reduce(
   (sum, currentProduct) => sum + currentProduct.price,
   startValue
 );
 console.log(sum);
-
-
 
 const elements = [1, 2, 3, 4, 5, 6];
 const startValue = [[], []];
