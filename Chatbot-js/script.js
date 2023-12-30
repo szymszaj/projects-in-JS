b13a83de8e762de1ffbc330649fc6eeae1d1dfbe
Szymon Zych <szymonzych868@gmail.com>
2023-12-30 21:30:34 +0100

    deleting api

--- Chatbot-js/script.js
+++ Chatbot-js/script.js
@@ -1,62 +1,67 @@
 const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
-const chatbox = document.querySelector(".chatbox"); 
+const chatbox = document.querySelector(".chatbox");
 
 let userMessage;
-const API_KEY = "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"
+const API_KEY = ''
 const createChatLi = (message, className) => {
-    const chatLi = document.createElement("li");
-    chatLi.classList.add("chat", className);
-    let chatContent = className === "outgoing" ? `<p>${message}</p>` : `<span><i class="fa-solid fa-robot"></i></span><p>${message}</p>`;
-    chatLi.innerHTML = chatContent;
-    return chatLi;
-}
+  const chatLi = document.createElement("li");
+  chatLi.classList.add("chat", className);
+  let chatContent =
+    className === "outgoing"
+      ? `<p>${message}</p>`
+      : `<span><i class="fa-solid fa-robot"></i></span><p>${message}</p>`;
+  chatLi.innerHTML = chatContent;
+  return chatLi;
+};
 
 const generateResponse = (incomingChatLi) => {
-    const API_URL = "https://api.openai.com/v1/chat/completions";
-    const messageElement = incomingChatLi.querySelector("p")
+  // const API_URL = "https://api.openai.com/v1/chat/completions";
+  const messageElement = incomingChatLi.querySelector("p");
 
-    const requestOptions = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${API_KEY}` 
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: "You are a helpful assistant.",
         },
-        body: JSON.stringify({
-            "model": "gpt-3.5-turbo",
-            "messages": [
-                {
-                    "role": "system",
-                    "content": "You are a helpful assistant."
-                },
-                {
-                    "role": "user",
-                    "content": userMessage  
-                }
-            ]
-        })
-    }
+        {
+          role: "user",
+          content: userMessage,
+        },
+      ],
+    }),
+  };
 
-    fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
-        messageElement.textContent = data.choices[0].message.content;
-    }).catch((error) => {
-        messageElement.textcontent = "oops! Something went wrong. Please try again.";
+  fetch(API_URL, requestOptions)
+    .then((res) => res.json())
+    .then((data) => {
+      messageElement.textContent = data.choices[0].message.content;
     })
-}
+    .catch((error) => {
+      messageElement.textcontent =
+        "oops! Something went wrong. Please try again.";
+    });
+};
 
 const handleChat = () => {
-    userMessage = chatInput.value.trim();
-    if(!userMessage) return;
+  userMessage = chatInput.value.trim();
+  if (!userMessage) return;
 
+  chatbox.appendChild(createChatLi(userMessage, "outgoing"));
 
-    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+  setTimeout(() => {
+    const incomingChatLi = createChatLi("Thinking...", "incoming");
+    chatbox.appendChild(incomingChatLi);
+    generateResponse(incomingChatLi);
+  }, 600);
+};
 
-    setTimeout(() => {
-        const incomingChatLi = createChatLi("Thinking...", "incoming")
-        chatbox.appendChild(incomingChatLi);
-        generateResponse(incomingChatLi);
-    }, 600);
-}
-
-sendChatBtn.addEventListener("click", handleChat)
-
+sendChatBtn.addEventListener("click", handleChat);
