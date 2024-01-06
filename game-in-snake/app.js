4ff1b9e893f83804d0048c7b9e161473f801eb6c
Szymon Zych <szymonzych868@gmail.com>
2024-01-06 16:50:19 +0100

    passing a random 0 -30 value as food position

---
+++ game-in-snake/app.js
@@ -0,0 +1,19 @@
+const playBoard = document.querySelector(".play-board");
+
+let foodX, foodY;
+let snakeX = 5,
+  snakeY = 10;
+
+const changeFoodPosition = () => {
+  //passing a random 0 -30 value as food position
+  foodX = Math.floor(Math.random() * 30) + 1;
+  foodY = Math.floor(Math.random() * 30) + 1;
+};
+const initGame = () => {
+  let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+  htmlMarkup += `<div class="food" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
+  playBoard.innerHTML = htmlMarkup;
+};
+
+changeFoodPosition();
+initGame();
