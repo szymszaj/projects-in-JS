aaaeac902fc0594022b2a676cee87ae5b665dcc6
Szymon Zych <szymonzych868@gmail.com>
2024-01-08 21:52:48 +0100

    new object

--- game-in-snake/app.js
+++ game-in-snake/app.js
@@ -4,10 +4,16 @@ let foodX, foodY;
 let snakeX = 5,
   snakeY = 10;
 
+let velocityX = 0,
+  velocityY = 0;
+
 const changeFoodPosition = () => {
-  //passing a random 0 -30 value as food position
   foodX = Math.floor(Math.random() * 30) + 1;
   foodY = Math.floor(Math.random() * 30) + 1;
+};
+
+const changeDirection = (e) => {
+  console.log(e);
 };
 const initGame = () => {
   let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
