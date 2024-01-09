2137732025a9847d3a054e239d3edff30bb014b2
Szymon Zych <szymonzych868@gmail.com>
2024-01-09 19:15:30 +0100

    moving a pawn around the map script

--- game-in-snake/app.js
+++ game-in-snake/app.js
@@ -12,12 +12,31 @@ const changeFoodPosition = () => {
   foodY = Math.floor(Math.random() * 30) + 1;
 };
 
+const changeDirection = (e) => {
+  if (e.key === "ArrowUp") {
+    velocityX = 0;
+    velocityY = -1;
+  } else if (e.key === "ArrowDown") {
+    velocityX = 0;
+    velocityY = 1;
+  } else if (e.key === "ArrowLeft") {
+    velocityX = -1;
+    velocityY = 0;
+  } else if (e.key === "ArrowRight") {
+    velocityX = 1;
+    velocityY = 0;
+  }
+  initGame();
+};
+
 const initGame = () => {
+  snakeX += velocityX;
+  snakeY += velocityY;
+
   let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
   htmlMarkup += `<div class="head" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
   playBoard.innerHTML = htmlMarkup;
 };
 
 changeFoodPosition();
-initGame();
 document.addEventListener("keydown", changeDirection);
