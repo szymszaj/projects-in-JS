6b1206c304385eef61e507e48a762f94b4f35a8e
Szymon Zych <szymonzych868@gmail.com>
2024-01-22 19:44:14 +0100

    removing a key-value pair from the hash table based on the provided key.

--- hash-table/script.js
+++ hash-table/script.js
@@ -11,9 +11,11 @@ class HashTable {
     this.storage = [];
     this.storageLimit = 14;
   }
+
   print() {
     console.log(this.storage);
   }
+
   add(key, value) {
     const index = hash(key, this.storageLimit);
     if (this.storage[index] === undefined) {
@@ -31,4 +33,40 @@ class HashTable {
       }
     }
   }
+
+  remove(key) {
+    const index = hash(key, this.storageLimit);
+    if (this.storage[index].length === 1 && this.storage[index][0][0] === key) {
+      delete this.storage[index];
+    } else {
+      for (let i = 0; i < this.storage[index].length; i++) {
+        if (this.storage[index][i][0] === key) {
+          delete this.storage[index][i];
+        }
+      }
+    }
+  }
+
+  lookup(key) {
+    const index = hash(key, this.storageLimit);
+    if (this.storage[index] === undefined) {
+      return undefined;
+    } else {
+      for (let i = 0; i < this.storage[index].length; i++) {
+        if (this.storage[index][i][0] === key) {
+          return this.storage[index][i][1];
+        }
+      }
+    }
+  }
 }
+
+console.log(hash("quincy", 10));
+
+const ht = new HashTable();
+ht.add("beau", "person");
+ht.add("fido", "dog");
+ht.add("rex", "dinosaur");
+ht.add("tux", "penguin");
+console.log(ht.lookup("tux"));
+ht.print();
