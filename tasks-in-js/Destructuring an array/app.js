9b0a9cb3b59796143c041c399878a1ec6dc0482e
Szymon Zych <szymonzych868@gmail.com>
2024-01-27 19:11:30 +0100

    Rest of the names

--- tasks-in-js/Destructuring an array/app.js
+++ tasks-in-js/Destructuring an array/app.js
@@ -9,7 +9,7 @@ console.log(name1, age1); // Anna 25
 console.log(name2, age2); // John 30
 console.log(name3, age3); // Eva 22
 
-const array = ["Anna", 25, "John", 30, "Eva", 22];
+// const array = ["Anna", 25, "John", 30, "Eva", 22];
 
 // Skipping names
 const [, age1, , age2, , age3] = array;
@@ -17,3 +17,10 @@ const [, age1, , age2, , age3] = array;
 console.log(age1); // 25
 console.log(age2); // 30
 console.log(age3); // 22
+const array = ["Anna", 25, "John", 30, "Eva", 22, "Christopher", 28];
+
+// Destructuring with the rest operator for names
+const [first_name, first_age, ...rest_names] = array;
+
+console.log(first_name, first_age); // Anna 25
+console.log(rest_names); // ["John", 30, "Eva", 22, "Christopher", 28]
