5ca746e938e776de1fa3b793d665c85fc2ed0bda
Szymon Zych <szymonzych868@gmail.com>
2024-01-27 19:11:05 +0100

    Skipping names and displaying only ages

--- tasks-in-js/Destructuring an array/app.js
+++ tasks-in-js/Destructuring an array/app.js
@@ -1,10 +1,19 @@
 // Sample array with names and numbers
-const array = ["Anna", 25, "John", 30, "Eva", 22];
+// const array = ["Anna", 25, "John", 30, "Eva", 22];
 
 // Destructuring the array
-const [name1, age1, name2, age2, name3, age3] = array;
+// const [name1, age1, name2, age2, name3, age3] = array;
 
 // Displaying the results
 console.log(name1, age1); // Anna 25
 console.log(name2, age2); // John 30
 console.log(name3, age3); // Eva 22
+
+const array = ["Anna", 25, "John", 30, "Eva", 22];
+
+// Skipping names
+const [, age1, , age2, , age3] = array;
+
+console.log(age1); // 25
+console.log(age2); // 30
+console.log(age3); // 22
