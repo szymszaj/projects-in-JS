4dd4654234a242e5c4ee8ac77e896236d12fe877
Szymon Zych <szymonzych868@gmail.com>
2024-01-28 18:35:30 +0100

    update

--- tasks-in-js/Destructuring an array/app.js
+++ tasks-in-js/Destructuring an array/app.js
@@ -1,16 +1,3 @@
-// Sample array with names and numbers
-// const array = ["Anna", 25, "John", 30, "Eva", 22];
-
-// Destructuring the array
-// const [name1, age1, name2, age2, name3, age3] = array;
-
-// Displaying the results
-console.log(name1, age1); // Anna 25
-console.log(name2, age2); // John 30
-console.log(name3, age3); // Eva 22
-
-// const array = ["Anna", 25, "John", 30, "Eva", 22];
-
 // Skipping names
 const [, age1, , age2, , age3] = array;
 
