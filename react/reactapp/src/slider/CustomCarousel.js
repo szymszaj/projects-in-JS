479b836a5b3b46522039d211d0755a0c96b4c5b8
Szymon Zych <szymonzych868@gmail.com>
2024-02-05 15:24:32 +0100

    Slider, a simple structure for navigating between images.

---
+++ react/reactapp/src/slider/CustomCarousel.js
@@ -0,0 +1,69 @@
+import Image from "next/image";
+import React, { useState } from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
+import styles from "./SimpleCarousel.module.css";
+
+const SimpleCarousel = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  return (
+    <div className={styles.carouselContainer}>
+      <div className={styles.carousel}>
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className={`${styles.slide} ${
+              index === currentIndex ? styles.activeSlide : ""
+            }`}
+          >
+            <Image
+              src={images[currentIndex]}
+              alt={`Slide ${currentIndex + 1}`}
+              layout="responsive"
+              width={600}
+              height={400}
+            />
+          </div>
+        ))}
+      </div>
+      <button
+        onClick={prevSlide}
+        className={styles.navButton}
+        style={{
+          left: "0%",
+        }}
+      >
+        <FaChevronLeft />
+      </button>
+      <button onClick={nextSlide} className={styles.navButton}>
+        <FaChevronRight />
+      </button>
+      <div className={styles.dotsContainer}>
+        {images.map((_, index) => (
+          <div
+            key={index}
+            className={`${styles.dot} ${
+              index === currentIndex ? styles.activeDot : ""
+            }`}
+            onClick={() => setCurrentIndex(index)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SimpleCarousel;
