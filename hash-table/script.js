a1f46cadabeb443cdb9954cf06de7f1a0e68e601
Szymon Zych <szymonzych868@gmail.com>
2024-02-13 22:42:49 +0100

    Performance and functional improvements in the HashTable.

--- hash-table/script.js
+++ hash-table/script.js
@@ -3,81 +3,89 @@ const hashFunction = (string, max) => {
   for (let i = 0; i < string.length; i++) {
     hash += string.charCodeAt(i);
   }
-  return hash % max; //Hash function - returns the remainder of the sum of character codes divided by the maximum limit
+  return hash % max;
 };
 
 class HashTable {
   constructor() {
-    this.storage = [];
-    this.storageLimit = 4; // Maximum limit for storing data in the hash table
+    this._storage = [];
+    this._storageLimit = 4;
   }
 
   print() {
-    console.log(this.storage);
+    console.log(this._storage);
   }
 
   add(key, value) {
-    const index = hashFunction(key, this.storageLimit);
-    if (this.storage[index] === undefined) {
-      this.storage[index] = [[key, value]];
+    const index = hashFunction(key, this._storageLimit);
+    if (this._storage[index] === undefined) {
+      this._storage[index] = [[key, value]];
     } else {
-      let inserted = false;
-      for (let i = 0; i < this.storage[index].length; i++) {
-        if (this.storage[index][i][0] === key) {
-          this.storage[index][i][1] = value;
-          inserted = true;
+      for (let i = 0; i < this._storage[index].length; i++) {
+        if (this._storage[index][i][0] === key) {
+          this._storage[index][i][1] = value;
+          return;
         }
       }
-      if (!inserted) {
-        this.storage[index].push([key, value]);
-        if (this.storage.length > this.storageLimit * 0.75) {
-          // Scaling up when the number of elements exceeds 75% of the storage size
-          this.resize();
-        }
+      this._storage[index].push([key, value]);
+      if (this._storage.length > this._storageLimit * 0.75) {
+        this.resize();
       }
     }
   }
 
   remove(key) {
-    const index = hashFunction(key, this.storageLimit);
-    if (this.storage[index].length === 1 && this.storage[index][0][0] === key) {
-      delete this.storage[index];
-    } else {
-      for (let i = 0; i < this.storage[index].length; i++) {
-        if (this.storage[index][i][0] === key) {
-          delete this.storage[index][i];
+    const index = hashFunction(key, this._storageLimit);
+    if (this._storage[index] === undefined) return;
+
+    for (let i = 0; i < this._storage[index].length; i++) {
+      if (this._storage[index][i][0] === key) {
+        this._storage[index].splice(i, 1);
+        if (this._storage[index].length === 0) {
+          delete this._storage[index];
         }
+        return;
       }
     }
   }
 
   lookup(key) {
-    const index = hashFunction(key, this.storageLimit);
-    if (this.storage[index] === undefined) {
-      return undefined;
-    } else {
-      for (let i = 0; i < this.storage[index].length; i++) {
-        if (this.storage[index][i][0] === key) {
-          return this.storage[index][i][1];
-        }
+    const index = hashFunction(key, this._storageLimit);
+    if (this._storage[index] === undefined) return undefined;
+
+    for (let i = 0; i < this._storage[index].length; i++) {
+      if (this._storage[index][i][0] === key) {
+        return this._storage[index][i][1];
       }
     }
   }
 
+  contains(key) {
+    const index = hashFunction(key, this._storageLimit);
+    if (this._storage[index] === undefined) return false;
+
+    for (let i = 0; i < this._storage[index].length; i++) {
+      if (this._storage[index][i][0] === key) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   iterate(callback) {
-    for (let i = 0; i < this.storage.length; i++) {
-      if (this.storage[i] !== undefined) {
-        for (let j = 0; j < this.storage[i].length; j++) {
-          callback(this.storage[i][j][0], this.storage[i][j][1]);
+    for (let i = 0; i < this._storage.length; i++) {
+      if (this._storage[i] !== undefined) {
+        for (let j = 0; j < this._storage[i].length; j++) {
+          callback(this._storage[i][j][0], this._storage[i][j][1]);
         }
       }
     }
   }
 
   resize() {
-    const oldStorage = this.storage;
-    this.storageLimit *= 2; // Doubling the maximum limit of the hash table's capacity
-    this.storage = [];
+    const oldStorage = this._storage;
+    this._storageLimit *= 2;
+    this._storage = [];
 
     oldStorage.forEach((bucket) => {
       if (bucket !== undefined) {
