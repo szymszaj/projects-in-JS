7333ba1565de4b81041eb3c8ed8c0b9020e83935
Szymon Zych <szymonzych868@gmail.com>
2024-02-17 13:55:39 +0100

    defining 2 classes

--- task-typescrip/index.ts
+++ task-typescrip/index.ts
@@ -1,3 +1,21 @@
-let name: string = "John";
-let age: number = 30;
-let isStudent: boolean = false;
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Student extends Person {
+  isStudent: boolean;
+
+  constructor(name: string, age: number, isStudent: boolean) {
+    super(name, age);
+    this.isStudent = isStudent;
+  }
+}
+
+let student1 = new Student("John", 20, true);
+let student2 = new Student("Alice", 22, true);
