68922d91bd88308606bbbc65f62fb3d4e10acee9
Szymon Zych <szymonzych868@gmail.com>
2024-02-26 17:10:37 +0100

    Updating the student's list of subjects.

--- task-typescrip/index.ts
+++ task-typescrip/index.ts
@@ -7,7 +7,6 @@ class Person {
     this.age = age;
   }
 
-  // Method to describe the person
   describe(): string {
     return `Name: ${this.name}, Age: ${this.age}`;
   }
@@ -17,21 +16,24 @@ class Student extends Person {
   isStudent: boolean;
   subjects: string[];
 
-  constructor(name: string, age: number, isStudent: boolean, subjects: string[]) {
+  constructor(
+    name: string,
+    age: number,
+    isStudent: boolean,
+    subjects: string[]
+  ) {
     super(name, age);
     this.isStudent = isStudent;
     this.subjects = subjects;
   }
 
-  // Method to add a subject
   addSubject(subject: string): void {
     this.subjects.push(subject);
   }
 
-  // Method to list subjects
   listSubjects(): void {
     console.log(`${this.name} is studying the following subjects:`);
-    this.subjects.forEach(subject => {
+    this.subjects.forEach((subject) => {
       console.log(subject);
     });
   }
@@ -41,10 +43,8 @@ class Student extends Person {
 let student1 = new Student("John", 20, true, ["Math", "Science"]);
 let student2 = new Student("Alice", 22, true, ["History", "English"]);
 
-// Adding a subject for student 1
 student1.addSubject("Computer Science");
 
-// Listing subjects for student 1
 student1.listSubjects();
 
 // Describing students
