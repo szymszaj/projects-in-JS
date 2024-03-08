a999c4c4043d0c830ffa2c88f62bf15bdbc5044f
Szymon Zych <szymonzych868@gmail.com>
2024-03-08 19:06:37 +0100

    The handling of clicks on list items has been updated and a visual effect has been added

--- decent_navigation/nav/app.js
+++ decent_navigation/nav/app.js
@@ -2,12 +2,18 @@ let marker = document.querySelector("#marker");
 let list = document.querySelectorAll("ul li");
 
 function moveIndicatore(e) {
-  marker.style.left = e.offsetLeft + "px";
-  marker.style.width = e.offsetWidth + "px";
+  marker.style.left = e.target.offsetLeft + "px";
+  marker.style.width = e.target.offsetWidth + "px";
 }
+
 list.forEach((link) => {
   link.addEventListener("click", (e) => {
-    moveIndicatore(e.target);
+    moveIndicatore(e);
   });
 });
 
+function activeLink() {
+  list.forEach((item) => item.classList.remove("active"));
+  this.classList.add("active");
+}
+list.forEach((item) => item.addEventListener("click", activeLink));
