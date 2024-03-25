54e5b9f74654cb06f399bb1eb12461dd21b3036a
Szymon Zych <szymonzych868@gmail.com>
2024-03-25 18:49:43 +0100

    Refactor  code for better readability

--- Projects/Magic-Ball/main.js
+++ Projects/Magic-Ball/main.js
@@ -1,42 +1,45 @@
-const ball = document.querySelector('img')
-const input = document.querySelector('input')
-const answer = document.querySelector('.answer')
-const error = document.querySelector('.error')
+const ball = document.querySelector("img");
+const input = document.querySelector("input");
+const answer = document.querySelector(".answer");
+const error = document.querySelector(".error");
 
-const answerArr = ['Tak!', 'Nie', 'po co chcesz to wiedzieć', ' nie wiem', 'Ciężko powiedzieć', 'Nie chcesz wiedzieć',
-    'sam to zrób', 'zapytaj mamy', 'nie lubie tego typu pytań', 'Może', 'chyba ty'];
+const answerArr = [
+  "Tak!",
+  "Nie",
+  "po co chcesz to wiedzieć",
+  " nie wiem",
+  "Ciężko powiedzieć",
+  "Nie chcesz wiedzieć",
+  "sam to zrób",
+  "zapytaj mamy",
+  "nie lubie tego typu pytań",
+  "Może",
+  "chyba ty",
+];
 
-
-
-    const shakeBall = () =>{
-        ball.classList.add('shake-animaton')
-        setTimeout(checkInput, 1000)
-    }
-
-const checkInput = () => {
-    if (input.value !== '' && input.value.slice(-1) === '?') {
-        generatorAnswer();
-        error.textContent = '';
-    } else if (input.value !== '' && input.value.slice(-1) !== '?') {
-        error.textContent = 'Pytanie musi być zakończone znakiem "?".';
-        answer.textContent = ''
-    } else {
-        error.textContent = 'Musisz zadać jakieś pytanie!';
-        answer.textContent = ''
-    }
-
-    ball.classList.remove('shake-animaton');
-}
-
-const generatorAnswer = () => {
-
-    const randomIndex = Math.floor(Math.random() * 11);
-    answer.innerHTML = `<span>Odpowiedz:</span> ${answerArr[randomIndex]}`
-
+const shakeBall = () => {
+  ball.classList.add("shake-animaton");
+  setTimeout(checkInput, 1000);
 };
 
-ball.addEventListener('click', shakeBall)
+const checkInput = () => {
+  if (input.value !== "" && input.value.slice(-1) === "?") {
+    generatorAnswer();
+    error.textContent = "";
+  } else if (input.value !== "" && input.value.slice(-1) !== "?") {
+    error.textContent = 'Pytanie musi być zakończone znakiem "?".';
+    answer.textContent = "";
+  } else {
+    error.textContent = "Musisz zadać jakieś pytanie!";
+    answer.textContent = "";
+  }
 
+  ball.classList.remove("shake-animaton");
+};
 
+const generatorAnswer = () => {
+  const randomIndex = Math.floor(Math.random() * 11);
+  answer.innerHTML = `<span>Odpowiedz:</span> ${answerArr[randomIndex]}`;
+};
 
-
+ball.addEventListener("click", shakeBall);
