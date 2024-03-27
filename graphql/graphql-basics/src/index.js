44c2b484705be648ac8e83642270f021e22b3a07
Szymon Zych <szymonzych868@gmail.com>
2024-03-27 12:12:02 +0100

    Added basic GraphQL server setup with schema and resolvers

--- graphql/graphql-basics/src/index.js
+++ graphql/graphql-basics/src/index.js
@@ -1,6 +1,43 @@
-import { GrapgQLServer } from "graphql-yoga";
+import { GraphQLServer } from "graphql-yoga";
 
-// Type definitions
+// Type definitions (schema)
+const typeDefs = `
+  type Query {
+    hello: String!
+    name: String!
+    id: String!
+    age: String!
+    location: String!
+  }
 
-
+`
 // Resolvers
+
+const resolvers = {
+  Query: {
+    hello() {
+      return "This is my first query!";
+    },
+    name(){
+      return 'Simon'
+    },
+    id(){
+      return 'm4d25896g458!2'
+    },
+    age(){
+      return '21'
+    },
+    location(){
+      return 'United Kingdom of Great Britain and Northern Ireland'
+    }
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+server.start(() => {
+  console.log("the server is up");
+});
