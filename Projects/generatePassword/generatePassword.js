6c160968e6ac2a6a7a1f3cc3b3fbc7e7d6fccdcf
Szymon Zych <szymonzych868@gmail.com>
2024-04-07 21:50:43 +0200

    Created password generator automation

---
+++ Projects/generatePassword/generatePassword.js
@@ -0,0 +1,15 @@
+function generatePassword(length) {
+  const charset =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+  return password;
+}
+
+const newPassword = generatePassword(12);
+console.log("Generated password:", newPassword);
+
+
