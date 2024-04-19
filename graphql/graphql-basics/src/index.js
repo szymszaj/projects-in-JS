4147f27f54e125ea3c4b9ca14e7c1c67a5198830
Szymon Zych <szymonzych868@gmail.com>
2024-04-19 08:58:02 +0200

    creating 1 board

--- graphql/graphql-basics/src/index.js
+++ graphql/graphql-basics/src/index.js
@@ -5,7 +5,8 @@ const typeDefs = `
 
   type Query {
     greeting(name: String!, position: String!): String!
-    add(a: Float!, b: Float!): Float!
+    add(numbers: [Float]!): Float!
+    grades: [Int!]!
     me: User!
     post: Post!
   }
@@ -34,9 +35,21 @@ const resolvers = {
         return "Hello!";
       }
     },
+
     add(parent, args, ctx, info) {
-      return args.a + args.b;
+      if (args.numbers.length === 0) {
+        return 0;
+      }
+
+      return args.numbers.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+      });
     },
+
+    grades(parent, args, ctx, info) {
+      return [99, 80, 93];
+    },
+
     me() {
       return {
         id: "123456",
@@ -45,6 +58,7 @@ const resolvers = {
         age: 28,
       };
     },
+
     post() {
       return {
         id: "25963",
