9d7b9ed7cc07ab641c0315ab7053f63210715277
Szymon Zych <szymonzych868@gmail.com>
2024-04-24 11:07:55 +0200

    Improves password strength display handling

---
+++ Checking-password-strength/app.js
@@ -0,0 +1,16 @@
+let passwordInput = document.getElementById("password");
+let passwordStrength = document.querySelectorAll("password-strength");
+let text = document.getElementById("text");
+
+passwordInput.addEventListener("input", function (event) {
+  let password = event.target.value;
+  let strength = Math.min(password.length, 12);
+  let degree = strength * 30;
+  let gradientColor =
+    strength <= 4 ? "#ff2c1c" : (strength <= 8 ? "#ff9800" : "#12ff12");
+
+  passwordStrength.forEach((passwordStrength) => {
+    passwordStrength.style.background = `conic-gradient(${gradientColor} ${degree}
+      deg, #1115 ${degree}deg)`;
+  });
+});
