28a2300ea211a1da97251dcd65bcb05f49bc0586
Szymon Zych <szymonzych868@gmail.com>
2024-04-25 22:22:18 +0200

    update script

--- Checking-password-strength/app.js
+++ Checking-password-strength/app.js
@@ -7,7 +7,7 @@ passwordInput.addEventListener("input", function (event) {
   let strength = Math.min(password.length, 12);
   let degree = strength * 30;
   let gradientColor =
-    strength <= 4 ? "#ff2c1c" : (strength <= 8 ? "#ff9800" : "#12ff12");
+    strength <= 4 ? "#ff2c1c" : strength <= 8 ? "#ff9800" : "#12ff12";
 
   passwordStrength.forEach((passwordStrength) => {
     passwordStrength.style.background = `conic-gradient(${gradientColor} ${degree}
