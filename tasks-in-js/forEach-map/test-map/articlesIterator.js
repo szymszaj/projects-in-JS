93eca2a14425435960171f0a5670eae70b047822
Szymon Zych <szymonzych868@gmail.com>
2024-04-28 21:06:46 +0200

    adding forEach/Added iteration through articles

--- tasks-in-js/forEach-map/test-map/articlesIterator.js
+++ tasks-in-js/forEach-map/test-map/articlesIterator.js
@@ -2,21 +2,25 @@ function generateLoremIpsum() {
   return "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 }
 
+function displayArticleInfo(article) {
+  console.log("Title: " + article.title);
+  console.log("Description: " + article.description);
+  console.log("---------------------");
+}
+
 let articles = [
-  { title: "Artykuł 1", description: generateLoremIpsum() },
-  { title: "Artykuł 2", description: generateLoremIpsum() },
-  { title: "Artykuł 3", description: generateLoremIpsum() },
-  { title: "Artykuł 4", description: generateLoremIpsum() },
-  { title: "Artykuł 5", description: generateLoremIpsum() },
-  { title: "Artykuł 6", description: generateLoremIpsum() },
-  { title: "Artykuł 7", description: generateLoremIpsum() },
-  { title: "Artykuł 8", description: generateLoremIpsum() },
-  { title: "Artykuł 9", description: generateLoremIpsum() },
+  { title: "Article 1", description: generateLoremIpsum() },
+  { title: "Article 2", description: generateLoremIpsum() },
+  { title: "Article 3", description: generateLoremIpsum() },
+  { title: "Article 4", description: generateLoremIpsum() },
+  { title: "Article 5", description: generateLoremIpsum() },
+  { title: "Article 6", description: generateLoremIpsum() },
+  { title: "Article 7", description: generateLoremIpsum() },
+  { title: "Article 8", description: generateLoremIpsum() },
+  { title: "Article 9", description: generateLoremIpsum() },
 ];
 
 articles.forEach(function (article, index) {
-  console.log("Artykuł " + (index + 1) + ":");
-  console.log("Tytuł: " + article.title);
-  console.log("Opis: " + article.description);
-  console.log("---------------------");
+  console.log("Article " + (index + 1) + ":");
+  displayArticleInfo(article);
 });
