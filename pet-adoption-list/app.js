28d6a5f4526623fba699c8e51327d622d1818848
Szymon Zych <szymonzych868@gmail.com>
2024-05-09 20:26:23 +0200

    correcting lines in code

--- pet-adoption-list/app.js
+++ pet-adoption-list/app.js
@@ -1,56 +1,58 @@
-const petPromise = await fetch("https://learnwebcode.github.io/pet-adoption-data/pets.json")
-const pets = await petPromise.json()
+const petPromise = await fetch(
+  "https://learnwebcode.github.io/pet-adoption-data/pets.json"
+);
+const pets = await petPromise.json();
 
-const template = document.querySelector("#animal-card")
-const wrapper = document.createElement("div")
+const template = document.querySelector("#animal-card");
+const wrapper = document.createElement("div");
 
-
-//wypisywanie  wieku 
+//wypisywanie  wieku
 function decideAgeText(age) {
-    if (!age) {
-        return "Less than a year old "
-    }
+  if (!age) {
+    return "Less than a year old ";
+  }
 
-    return age > 1 ? `${age} years old ` : "1 year old "
+  return age > 1 ? `${age} years old ` : "1 year old ";
 }
 
-pets.forEach(pet => {
+pets.forEach((pet) => {
+  //odpowiednie/zmiana nazwy
+  const clone = template.content.cloneNode(true);
+  clone.querySelector("h3").textContent = pet.name;
 
-    //odpowiednie/zmiana nazwy
-    const clone = template.content.cloneNode(true)
-    clone.querySelector("h3").textContent = pet.name
+  //zmiana zdjecia
+  const img = clone.querySelector("img");
+  img.src = pet.photo;
+  img.alt = `A ${pet.species} named ${pet.name}`;
 
-    //zmiana zdjecia 
-    const img = clone.querySelector("img")
-    img.src = pet.photo
-    img.alt = `A ${pet.species} named ${pet.name}`
+  //wywolywanie wieku
+  const age = new Date().getFullYear() - pet.birthYear;
+  const ageText = decideAgeText(age);
+  clone.querySelector(".age").textContent = ageText;
 
-    //wywolywanie wieku
-    const age = new Date().getFullYear() - pet.birthYear
-    const ageText = decideAgeText(age)
-    clone.querySelector(".age").textContent = ageText
+  clone.querySelector(".species").textContent = pet.species;
+  clone.querySelector(".description").textContent = pet.description;
+  clone.querySelector(".name").textContent = pet.name;
+  clone.querySelector(
+    ".primary-btn"
+  ).href = `https://learnwebcode.github.io/pet-adoption-data/pets/${pet.id}/`;
 
-    clone.querySelector(".species").textContent = pet.species
-    clone.querySelector(".description").textContent = pet.description
-    clone.querySelector(".name").textContent = pet.name
-    clone.querySelector(".primary-btn").href = `https://learnwebcode.github.io/pet-adoption-data/pets/${pet.id}/`
+  wrapper.appendChild(clone);
+});
 
-    wrapper.appendChild(clone)
-})
+document.querySelector(".animals").appendChild(wrapper);
 
-document.querySelector(".animals").appendChild(wrapper)
-
-const filterButtons = document.querySelectorAll(".filter-nav a")
-filterButtons.forEach(el => {
-    el.addEventListener("click", e => handleFilterClick(e))
-})
+const filterButtons = document.querySelectorAll(".filter-nav a");
+filterButtons.forEach((el) => {
+  el.addEventListener("click", (e) => handleFilterClick(e));
+});
 
 function handleFilterClick(e) {
-    let target = e.target
+  let target = e.target;
 
-    e.preventDefault()
-    filterButtons.forEach(el => {
-        el.classList.remove("active")
-    })
-    target.classList.add("active")
+  e.preventDefault();
+  filterButtons.forEach((el) => {
+    el.classList.remove("active");
+  });
+  target.classList.add("active");
 }
