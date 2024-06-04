eea747960341472e726c03951caed3f0e5410520
Szymon Zych <szymonzych868@gmail.com>
2024-06-05 00:20:03 +0200

    removing unnecessary code

--- creating firework effect/index.js
+++ creating firework effect/index.js
@@ -1,27 +1,22 @@
-// Creating fireworks
 function createFirework() {
-    // Create a <div> element for the firework
-    var firework = document.createElement("div");
-    firework.className = "firework";
-    
-    // Generate a random position for the firework on the screen
-    var left = Math.random() * window.innerWidth + "px";
-    var top = Math.random() * window.innerHeight + "px";
-    firework.style.left = left;
-    firework.style.top = top;
-    
-    // Add a firework to the body of the document
-    document.body.appendChild(firework);
-    
-    // Remove the firework after the animation ends
-    firework.addEventListener("animationend", function() {
-      firework.parentNode.removeChild(firework);
-    });
-  }
-  
-  // Handling the button click event
-  document.getElementById("fireworkButton").addEventListener("click", function() {
-    // Create 10 fireworks
+  var firework = document.createElement("div");
+  firework.className = "firework";
+
+  var left = Math.random() * window.innerWidth + "px";
+  var top = Math.random() * window.innerHeight + "px";
+  firework.style.left = left;
+  firework.style.top = top;
+
+  document.body.appendChild(firework);
+
+  firework.addEventListener("animationend", function () {
+    firework.parentNode.removeChild(firework);
+  });
+}
+
+document
+  .getElementById("fireworkButton")
+  .addEventListener("click", function () {
     for (var i = 0; i < 100; i++) {
       createFirework();
     }
