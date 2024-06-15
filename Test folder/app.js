cae0ab0e1d0cfd513ce7749e4713b0b94ff475dd
Szymon Zych <szymonzych868@gmail.com>
2024-06-15 22:48:00 +0200

    deleting test files JS

--- Test folder/app.js
+++
@@ -1,5 +0,0 @@
-function reverseWord() {
-    const input = document.getElementById('word');
-    const reversedWord = input.value.split('').reverse().join('');
-    document.getElementById('reversed').textContent = reversedWord;
-  }
