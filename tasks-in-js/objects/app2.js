b1c32e13f3b04ada94502903501d74383fb188e5
Szymon Zych <szymonzych868@gmail.com>
2024-06-22 21:00:41 +0200

    refactoring

--- tasks-in-js/objects/app2.js
+++ tasks-in-js/objects/app2.js
@@ -1,46 +1,37 @@
-//adding things to objects
-
 const user = {
-    name: 'Justyna',
-    age: 20,
-    'fav-color': 'czerwony'
-}
-user.country = 'Polska'
-//user.fav-color
-user['fav-color'] = 'niebieski'
+  name: "Justyna",
+  age: 20,
+  "fav-color": "czerwony",
+};
+user.country = "Polska";
+user["fav-color"] = "niebieski";
 console.log(user);
 
-//proba 1
-const dogName = 'dante'
-const dogAge = 4
+const dogName = "dante";
+const dogAge = 4;
 
 const dog = {
-    dogName,
-    dogAge
-
-}
+  dogName,
+  dogAge,
+};
 console.log(dog);
 
-//proba 2
-
-const birdType = 'parrot'
-const birdColor = 'black'
-const birdAge = 2
+const birdType = "parrot";
+const birdColor = "black";
+const birdAge = 2;
 
 const bird = {
-    birdType,
-    birdColor,
-    birdAge
-}
+  birdType,
+  birdColor,
+  birdAge,
+};
 console.log(bird);
 
-//proba 3
-
-const catName = 'miss'
-const catAge = 5
+const catName = "miss";
+const catAge = 5;
 
 const cat = {
-    catName,
-    catAge
-}
+  catName,
+  catAge,
+};
 console.log(cat);
